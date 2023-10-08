043ef45ad7141d46330aea35a834b2e96ff6490f
h3ll3r9 <20051511@kiit.ac.in>
2023-10-08 18:34:53 +0530

    Time addded

--- whatsappspammer.py
+++ whatsappspammer.py
@@ -6,6 +6,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 import time
+def send_message(message, action, cursor):
+    action.click(cursor).perform()
+    cursor.send_keys(message)
+    cursor.send_keys(Keys.RETURN)
 driver=webdriver.Chrome('C:\Program Files (x86)\chromedriver.exe')
 driver.get('https://web.whatsapp.com/')
 action=ActionChains(driver)
@@ -17,7 +21,8 @@ driver.implicitly_wait(5)
 path = input("Enter xpath")
 cursor=driver.find_element_by_xpath(xpath)
 message = input("Enter the message")
-action.click(cursor).perform()
-for x in range(50):
-    cursor.send_keys(message)
-    cursor.send_keys(Keys.RETURN)
+now = time.localtime()
+next_run_time = time.mktime(time.replace(now, hour=12, minute=0, second=0))
+time_to_sleep = next_run_time - time.time()
+time.sleep(time_to_sleep)
+send_message(message, action, cursor)
